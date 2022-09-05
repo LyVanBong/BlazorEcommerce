b2fc5130ae0c9f29e2ecf6f115fdf451caac5d8b
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-09-06 00:38:14 +0700

    merge code

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,3 +1,6 @@
+using BlazorEcommerce.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
+});
 
 var app = builder.Build();
 
--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorEcommerce.Shared;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorEcommerce.Shared;
 
 public class Product
 {
@@ -6,5 +8,6 @@ public class Product
     public string Title { get; set; } = String.Empty;
     public string Description { get; set; } = String.Empty;
     public string ImageUrl { get; set; } = String.Empty;
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 }
